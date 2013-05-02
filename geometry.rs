63faf1c29363599331a704d9265b3493a4d87375
Olivier Saut <osaut@airpost.net>
2013-05-02 23:44:25 +0200

    Rajout de l'opérateur de division

--- geometry.rs
+++ geometry.rs
@@ -71,26 +71,28 @@ impl<Result, Rhs: RhsOfMul<Result> > ops::Mul<Rhs, Result> for Point {
   }
 }
 // Division
-// trait RhsOfDiv<Result> {
-//   fn div_lhs_to(&self, lhs: &Point) -> Result;
-// }
-// impl RhsOfDiv<Point> for Point {
-//   fn div_lhs_to(&self, lhs: &Point) -> Point {
-//     Point{x: (self.x/lhs.x), y:(self.y/lhs.y)}
-//    }
-// }
-// impl RhsOfDiv<Point> for f64 {
-//   fn div_lhs_to(&self, lhs: &Point) -> Point {
-//     Point{x: (lhs.x/self), y:(lhs.y/self)}
-//    }
-// }
-// impl<Result, Rhs: RhsOfDiv<Result> > ops::Div<Rhs, Result> for Point {
-//   fn div(&self, rhs: &Rhs) -> Result {
-//     rhs.div_lhs_to(self)
-//   }
-// }
+trait RhsOfDiv<Result> {
+  fn div_lhs_to(&self, lhs: &Point) -> Result;
+}
+impl RhsOfDiv<Point> for Point {
+  fn div_lhs_to(&self, lhs: &Point) -> Point {
+    Point{x: (lhs.x/self.x), y:(lhs.y/self.y)}
+   }
+}
+impl RhsOfDiv<Point> for f64 {
+  fn div_lhs_to(&self, lhs: &Point) -> Point {
+    Point{x: (lhs.x/ *self), y:(lhs.y/ *self)}
+   }
+}
+impl<Result, Rhs: RhsOfDiv<Result> > ops::Div<Rhs, Result> for Point {
+  fn div(&self, rhs: &Rhs) -> Result {
+    rhs.div_lhs_to(self)
+  }
+}
 
-// Test
+//
+// * Test
+//
 #[test]
 fn test_dist() {
     let pt1=@Point{ x: 1.0, y: 2.0};
@@ -127,15 +129,15 @@ fn test_mult() {
     assert!((pt4.x==2.0)&&(pt4.y==4.0));
 }
 
-// #[test]
-// fn test_div() {
-//     let pt1=@Point{ x: 1.0, y: 2.0};
-//     let pt2=@Point{ x: -1.0, y: 3.0};
-//     let pt3=(*pt1)/(*pt2);
-//     assert!((pt3.x==-1.0)&&(pt3.y==-2.0/3.0));
-//     let pt4=(*pt1)/(2.0 as f64);
-//     assert!((pt4.x==0.5)&&(pt4.y==1.0));
-// }
+#[test]
+fn test_div() {
+    let pt1=@Point{ x: 1.0, y: 2.0};
+    let pt2=@Point{ x: -1.0, y: 3.0};
+    let pt3=(*pt1)/(*pt2);
+    assert!(pt3.dist(&Point{x:-1.0, y:2.0/3.0})==0.0);
+    let pt4=(*pt1)/(2.0 as f64);
+    assert!((pt4.x==0.5)&&(pt4.y==1.0));
+}
 
 #[test]
 fn test_rand() {
