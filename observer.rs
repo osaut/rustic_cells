3ac37cd88123bd83d7ed263b9d006fbf20016c29
Olivier Saut <osaut@airpost.net>
2013-05-07 14:13:15 +0200

    Sortie sur le disque

--- observer.rs
+++ observer.rs
@@ -5,7 +5,7 @@ mod agent;
 
 // Type Observer
 pub trait Observer {
-    fn see(&self, time: f64, crowd: &Crowd);
+    fn see(&mut self, time: f64, crowd: &Crowd);
     fn request_at(&mut self, time: f64) -> bool;
 }
 
@@ -26,7 +26,7 @@ pub impl ScreenPrinter {
 }
 
 impl Observer for ScreenPrinter {
-    fn see(&self, time: f64, crowd: &Crowd) {
+    fn see(&mut self, time: f64, crowd: &Crowd) {
         io::println(fmt!("%f\n", time as float));
         io::println(crowd.to_str());
     }
@@ -43,18 +43,45 @@ impl Observer for ScreenPrinter {
 struct DiskWriter {
     freq: uint,
     fname: ~str,
+    curr_snap: uint,
     ticks: uint
 }
 
 pub impl DiskWriter {
     fn new(freq:uint, name: ~str) -> DiskWriter {
-        DiskWriter {freq: freq, fname: name, ticks: 0}
+        DiskWriter {freq: freq, fname: copy name, curr_snap: 0, ticks: 0}
+    }
+
+    fn new_filename(&mut self) -> ~str {
+        self.curr_snap+=1;
+        fmt!("./%s-%u.vtk", self.fname, self.curr_snap-1)
     }
 }
 
 impl Observer for DiskWriter {
-    fn see(&self, time: f64, crowd: &Crowd) {
-      io::println(fmt!("%f : %d cells -> %s.\n",time as float, crowd.size() as int, self.fname));
+    fn see(&mut self, time: f64, crowd: &Crowd) {
+        io::println(fmt!("%f : %d cells -> %s.\n",time as float, crowd.size() as int, self.fname));
+        let outfile=self.new_filename();
+        let writer = result::get( &io::file_writer( &Path(outfile), [io::Create, io::Truncate] ) );
+
+        // Ecriture de l'entête
+        writer.write_str("# vtk DataFile Version 3.0\n");
+        writer.write_str("Rusty_Cells\n");
+        writer.write_str("ASCII\n");
+        writer.write_str("DATASET UNSTRUCTURED_GRID\n");
+        writer.write_str(fmt!("POINTS %u float\n", crowd.size()));
+
+
+        // Coordonnées des cellules
+        for crowd.cells.each |&cell| {
+            writer.write_str(fmt!("%f %f 0\n",cell.x() as float, cell.y() as float))
+        }
+
+        // Données sur les nœuds
+        writer.write_str(fmt!("POINT_DATA %u\nSCALARS %s float\nLOOKUP_TABLE default", crowd.size(), self.fname));
+        for crowd.cells.each |&cell| {
+            writer.write_str(fmt!("%u\n", cell.id));
+        }
     }
 
     fn request_at(&mut self, time: f64) -> bool {
--- run.rs
+++ run.rs
@@ -11,13 +11,16 @@ fn main() {
     io::println(cells.to_str());
 
     let mut obs = ScreenPrinter::new(10);
-
+    let mut obs2 = DiskWriter::new(10, ~"cells");
     let dt=0.001;
     let mut t=0.0;
     for 100.times {
         cells.evolve(dt);
         if(obs.request_at(t)) {
             obs.see(t, &cells);
+        }
+        if(obs2.request_at(t)) {
+            obs2.see(t, &cells);
         }
         t+=dt;
     }
