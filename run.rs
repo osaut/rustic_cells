53812c15b3d3d8ddd0dfb8b82672d672050bf60d
Olivier Saut <osaut@airpost.net>
2013-05-09 16:54:49 +0200

    Sortie sur le disque

--- run.rs
+++ run.rs
@@ -7,14 +7,13 @@ mod observer;
 
 fn main() {
 
-    let cells=Crowd::new(10);
-    io::println(cells.to_str());
+    let mut cells=Crowd::new(100);
 
-    let mut obs = ScreenPrinter::new(10);
-    let mut obs2 = DiskWriter::new(10, ~"cells");
+    let mut obs = ProgressMeter::new(200);
+    let mut obs2 = DiskWriter::new(200, ~"cells");
     let dt=0.001;
     let mut t=0.0;
-    for 100.times {
+    for 10000.times {
         cells.evolve(dt);
         if(obs.request_at(t)) {
             obs.see(t, &cells);
